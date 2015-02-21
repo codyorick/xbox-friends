b43fcff7730b70634bad437f5a7d2e2246fb1b36
codyorick <codyorick@gmail.com>
2015-02-20 20:49:39 -0500

    back end working

--- xbox.py
+++ xbox.py
@@ -1,6 +1,6 @@
 import sys
 import requests
-import pymongo
+from pymongo import MongoClient
 
 XUID_API = 'https://xboxapi.com/v2/xuid/'
 BASE_API = 'https://xboxapi.com/v2/'
@@ -32,8 +32,44 @@ def main():
 
     friends = friends.json()
 
+    friends_record = {}
+
+    friends_record['gamertag'] = user.lower()
+
+    friends_list = []
+
     for f in friends:
-        print f['Gamertag']
+        friends_list.append(f['Gamertag'])
+
+    friends_record['friends'] = friends_list
+
+    client = MongoClient()
+    db = client.xboxdb
+    results = db.friendlists.find( { 'gamertag' : friends_record['gamertag'] } )
+    if results.count() == 0:
+        print "user not found, adding user..."
+        db.friendlists.insert(friends_record)
+        sys.exit(0)
+    else:
+        print "user found"
+        old_list = set(results[0]['friends'])
+        new_list = set(friends_list)
+        removed = old_list - new_list
+        added = new_list - old_list
+        if not removed and not added:
+            print "no new or removed friends"
+            sys.exit(0)
+        if removed:
+            print "removals: "
+            for f in removed:
+                print f
+        if added:
+            print "new friends: "
+            for f in added:
+                print f
+        db.friendlists.update({'gamertag' : friends_record['gamertag']}, {'$set' : {'friends' : friends_list}})
+        print 'updated record'
+        sys.exit(0)
 
 
 if __name__ == "__main__":
